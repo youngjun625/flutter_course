c745f71572cff115c206bd17dc2b0003bce737f9
YJ's MacBook Pro <youngjun625@hotmail.com>
2024-10-27 16:55:11 +0900

    assignment6

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,16 @@ class MainApp extends StatelessWidget {
     return  MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              Image.asset("assets/images/boy.png"),
-              Text(
-                "영도체 레귤러",
-                style: TextStyle(
-                  fontFamily: "Yeongdo",
-                  fontSize: 24,
-                  fontWeight: FontWeight.w400
-                ),
-              ),
-              Text(
-                "영도체 볼드",
-                style: TextStyle(
-                  fontFamily: "Yeongdo",
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700
-                ),
-              ),
-              Text(
-                "영도체 헤비",
-                style: TextStyle(
-                  fontFamily: "Yeongdo",
-                  fontSize: 24,
-                  fontWeight: FontWeight.w900
-                ),
-              )
-            ],
-          )
+          child: GestureDetector(
+            onTap: () {
+              print('Container Tapped');
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+          ),
         )
       ),
     );
