9db8772cc9a9997a8b6f025f1d85f880d6340d5d
YJ's MacBook Pro <youngjun625@hotmail.com>
2024-11-04 10:50:01 +0900

    assignment8

--- lib/main.dart
+++ lib/main.dart
@@ -4,85 +4,31 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
+    int _counter = 0;
     return  MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
             children: [
-              Stack(
-                children: [
-                  Image.asset("assets/images/boy.png"),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.width,
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                  Positioned(
-                    top: 12,
-                    right: 12,
-                    child: Text(
-                      "안녕하세요",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: "Yeongdo",
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700
-                      ),
-                    ),
-                  )
-                ],
-              ), 
-              SizedBox(height: 12,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: Row(
-                  children: [
-                      Text("이름",
-                          style: TextStyle(
-                              fontFamily: "Yeongdo",
-                              fontSize: 24,
-                              fontWeight: FontWeight.w700
-                          ),
-                      ),
-                      Spacer(),
-                      Text("김민혁",
-                          style: TextStyle(
-                              fontFamily: "Yeongdo",
-                              fontSize: 24,
-                              fontWeight: FontWeight.w700
-                          ),
-                      ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 12,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: Row(
-                  children: [
-                      Text("나이",
-                          style: TextStyle(
-                              fontFamily: "Yeongdo",
-                              fontSize: 24,
-                              fontWeight: FontWeight.w700
-                          ),
-                      ),
-                      Spacer(),
-                      Text("18세",
-                          style: TextStyle(
-                              fontFamily: "Yeongdo",
-                              fontSize: 24,
-                              fontWeight: FontWeight.w700
-                          ),
-                      ),
-                  ],
-                ),
-              )
+                Text(_counter.toString()),
+                GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _counter++;
+                      });
+                    },
+                    child: const Icon(Icons.add)
+                )
             ],
           )
         )
